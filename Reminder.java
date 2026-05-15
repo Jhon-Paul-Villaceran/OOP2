b451800c63ac499f004a4c54df40560196d70012
John Leonard Cataroja <136213587+JohnLeonardCataroja@users.noreply.github.com>
2026-05-16 01:16:15 +0800

    feat: implement Reminder class logic

--- Reminder.java
+++ Reminder.java
@@ -1,1 +1,19 @@
+package com.studytracker.model;
 
+import java.time.LocalTime;
+
+public class Reminder{
+    public int id;
+    public int userID;
+    public String subject;
+    public LocalTime time;
+    public String message;
+    
+    public Reminder(int id, int userID, String subject, LocalTime time, String message){
+        this.id = id;
+        this.userID = userID;
+        this.subject = subject;
+        this.time = time;
+        this.message = message;
+    }
+}
